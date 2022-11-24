d4c245f057123abd3ce80a0ad2af3e0430162c90
sherin stephen <sherin@sherins-MacBook-Pro.local>
2022-11-24 00:32:00 -0800

    nvim from scratch, remove others

---
+++ nvim/lua/user/options.lua
@@ -0,0 +1,13 @@
+vim.opt.tabstop = 2                             -- insert 2 spaces for a tab
+vim.opt.cursorline = true                       -- highlight the current line
+vim.opt.number = true   			                  -- set numbered lines
+vim.opt.relativenumber = true                  	-- set relative numbered lines
+vim.opt.cmdheight = 2                           -- more space in the neovim command line for displaying messages
+vim.opt.fileencoding = "utf-8"                  -- the encoding written to a file
+vim.opt.mouse = "a"                             -- allow the mouse to be used in neovim
+vim.opt.smartcase = true                        -- smart case
+vim.opt.smartindent = true                      -- make indenting smarter again
+vim.opt.expandtab = true                        -- convert tabs to spaces
+vim.opt.termguicolors = true                    -- set term gui colors (most terminals support this)
+vim.opt.cmdheight = 2                           -- more space in the neovim command line for displaying messages
+
